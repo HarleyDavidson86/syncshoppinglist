bdcabcbe65d219e1657fedbb2c3657ffe29f18eb
Dominik Sust <dominik.sust@gmail.com>
2024-12-31 17:55:11 +0100

    Added icon button

--- lib/screens/home/shoppingitem_card.dart
+++ lib/screens/home/shoppingitem_card.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:syncshoppinglist/shared/styled_text.dart';
+import 'package:syncshoppinglist/theme.dart';
 
 class ShoppingitemCard extends StatelessWidget {
   const ShoppingitemCard(this.name, {super.key});
@@ -9,10 +11,15 @@ class ShoppingitemCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 20),
+        padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 20),
         child: Row(
           children: [
-            Text(name),
+            StyledText(name),
+            const Expanded(child: SizedBox()),
+            IconButton(
+              onPressed: (){}, 
+              icon: Icon(Icons.shopping_cart, color: AppColors.textColor),
+            ),
           ],
         ),
       ),
